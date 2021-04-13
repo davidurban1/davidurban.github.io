e6e7ce7f1c3197c49ff0d60b5a06942afcc4b631
davidurban1 <74219572+davidurban1@users.noreply.github.com>
2021-04-12 22:57:53 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,10 @@
+// @ts-ignore
+firebase.initializeApp({
+   apiKey: "AIzaSyCldDPPu7CPT9pAD2TYrTpHHmxexp9StM0",
+    authDomain: "iotadangomez-1d5e0.firebaseapp.com",
+    projectId: "iotadangomez-1d5e0",
+    storageBucket: "iotadangomez-1d5e0.appspot.com",
+    messagingSenderId: "1028950036316",
+    appId: "1:1028950036316:web:1ed887672cbcec36a87b99"
+
+});
